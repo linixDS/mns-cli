ad2a4a07616be91505099b55da715c3c96ddb94b
Dariusz Marcisz <dmarcisz.ds@gmail.com>
2023-04-26 13:47:26 +0200

    New Module

---
+++ mikrotik/mikrotik.cs
@@ -0,0 +1,34 @@
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using Core;
+
+namespace module
+{
+    public class HELPCLASS
+    {
+        public HELPCLASS()
+        {
+            Console.Clear();
+            Console.WriteLine("NAME");
+            Console.WriteLine("\t MikroTik Managment ");
+            Console.WriteLine("");
+            Console.WriteLine("SHORT DESCRIPTION");
+            Console.WriteLine("\");
+            Console.WriteLine("");
+            Console.WriteLine("SYNTEX");
+            Console.WriteLine("\t mikrotik help");
+            Console.WriteLine("");
+        }
+    }
+
+
+
+
+
+
+}
