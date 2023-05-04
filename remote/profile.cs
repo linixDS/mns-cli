a65995e9e5f4aae8d2842aa0b220a8434b33444b
Dariusz Marcisz <dmarcisz.ds@gmail.com>
2023-05-04 19:00:13 +0200

    New version module remote(add ssh client)

---
+++ remote/profile.cs
@@ -0,0 +1,144 @@
+﻿using Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace remote
+{
+    public class CONFIGCLASS
+    {
+        public int Port { get; set; }
+        public string Address { get; set; }
+        public string User { get; set; }
+        public string Password { get; set; }
+
+        public CONFIGCLASS()
+        {
+            this.Port= 22;
+            this.Address = string.Empty;
+            this.User = "admin";
+            this.Password = string.Empty;
+        }
+    }
+
+    public class PROFILECLASS
+    {
+        public void help()
+        {
+            Console.Clear();
+            Console.WriteLine("NAME");
+            Console.WriteLine("\t Profile configuration device");
+            Console.WriteLine("");
+            Console.WriteLine("SHORT DESCRIPTION");
+            Console.WriteLine("");
+            Console.WriteLine("SYNTEX");
+            Console.WriteLine("\t remote profile help");
+            Console.WriteLine("\t remote profile list");
+            Console.WriteLine("\t remote profile remove <input: name>");
+            Console.WriteLine("\t remote profile add <input: name>");
+            Console.WriteLine("");
+        }
+
+        public void list()
+        {
+            Terminal.WriteText("::List all profiles in remote module", ConsoleColor.Green, Console.BackgroundColor);
+            string[] files =  Directory.GetFiles(Directory.GetCurrentDirectory());
+
+            foreach (string file in files)
+            {
+                string ext = Path.GetExtension(file);
+                if (ext.Contains(".profile"))
+                {
+                    Console.Write("Profile name: ");
+                    Terminal.WriteText(Path.GetFileNameWithoutExtension(file), ConsoleColor.Yellow, Console.BackgroundColor);
+                }
+            }
+        }
+
+        public void remove(string name)
+        {
+            var fileName = name + ".profile";
+            if (!File.Exists(fileName))
+            {
+                Terminal.ErrorWrite("Error: No found profile: " + name);
+                return;
+            }
+
+            try
+            {
+                File.Delete(fileName);
+                Terminal.WriteText("Deleted profile.", ConsoleColor.Green, Console.BackgroundColor);
+            }
+            catch (Exception error)
+            {
+                Terminal.ErrorWrite("Error: " + error.Message);
+            }
+        }
+
+        public void add(string name)
+        {
+            var config = new CONFIGCLASS();
+            var fileName = name + ".profile";
+            if (File.Exists(fileName))
+            {
+                Terminal.ErrorWrite("Error: Profile already exists: " + name) ;
+                return;
+            }
+
+            try
+            {
+                Console.Write("ADDRESS> ");
+                config.Address = Console.ReadLine();
+                Console.Write("PORT> ");
+                string value = Console.ReadLine();
+                config.Port = Int32.Parse(value);
+                Console.Write("USER> ");
+                config.User = Console.ReadLine();
+
+                Console.Write("PASSWORD> ");
+                config.Password = Console.ReadLine();
+                if (config.Password.Length > 0)
+                    config.Password = Core.Crypto.Encrypt(config.Password, "hoff01HOFF02");
+
+
+
+                var json = JsonSerializer.Serialize(config);
+
+                StreamWriter writer = new StreamWriter(fileName);
+                writer.WriteLine(json);
+                writer.Close();
+                Terminal.WriteText("Saved new profile.", ConsoleColor.Green, Console.BackgroundColor);
+            }
+            catch (Exception error)
+            {
+                Terminal.ErrorWrite("Error: " + error.Message);
+            }
+            Console.WriteLine();
+        }
+
+
+        public CONFIGCLASS load(string name)
+        {
+            try
+            {
+                var fileName = name + ".profile";
+                if (!File.Exists(fileName))  return null;
+
+                string json = File.ReadAllText(fileName);
+                var config = JsonSerializer.Deserialize<CONFIGCLASS>(json);
+                if (config.Password.Length > 1)
+                    config.Password = Core.Crypto.Decrypt(config.Password, "hoff01HOFF02");
+
+                return config;
+            }
+            catch (Exception error)
+            {
+                Terminal.ErrorWrite("Error: " + error.Message);
+                return null;
+            }
+        }
+    }
+}
--- remote/remote.cs
+++ remote/remote.cs
@@ -24,12 +24,72 @@ namespace remote
             Console.WriteLine("");
             Console.WriteLine("SYNTEX");
             Console.WriteLine("\t remote help");
+            Console.WriteLine("\t remote profile");
             Console.WriteLine("\t remote vnc");
             Console.WriteLine("\t remote rdp");
             Console.WriteLine("\t remote pssession");
             Console.WriteLine("");
         }
     }
+
+    public class SSHCLASS
+    {
+        public void help()
+        {
+            Console.Clear();
+            Console.WriteLine("NAME");
+            Console.WriteLine("\t Remote SSH Client");
+            Console.WriteLine("");
+            Console.WriteLine("SHORT DESCRIPTION");
+            Console.WriteLine("\t");
+            Console.WriteLine("");
+            Console.WriteLine("SYNTEX");
+            Console.WriteLine("\t remote ssh help");
+            Console.WriteLine("\t remote ssh connect <input1: ip address> <input2: port> <input3: user>");
+            Console.WriteLine("\t remote ssh load <input: profile>");
+            Console.WriteLine("");
+        }
+
+        public void connect(string address, string port, string user)
+        {
+            Terminal.WriteText("::Connecting to SSH server: " + address+":"+port, ConsoleColor.Green, Console.BackgroundColor);
+            try
+            {
+                string strCmdText;
+                string command;
+                strCmdText = String.Format("{0}@{1} -p {2}",user,address,port);
+                if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+                {
+                    command = "ssh.exe";
+                }
+                else
+                {
+                    command = "/usr/bin/ssh";
+                }
+
+                Terminal.ExecuteProcess(command, strCmdText);
+            }
+            catch (Exception error)
+            {
+                Terminal.ErrorWrite("Error: " + error.Message);
+            }
+            Console.WriteLine();
+        }
+
+        public void load(string profileName)
+        {
+            var profile = new PROFILECLASS();
+            var config = profile.load(profileName);
+            if (config == null)
+            {
+                Terminal.ErrorWrite("No found profile: " + profileName);
+                return;
+            }
+
+            connect(config.Address, config.Port.ToString(), config.User);
+        }
+    }
+
 
     public class VNCCLASS
     {
